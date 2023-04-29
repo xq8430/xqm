82e0266616c52031300463fcfbac6be3b81f2dbb
xuqiang <xq8430@163.com>
2023-04-29 15:35:02 +0900

    init

---
+++ src/main/java/com/example/xqm/XqmApplication.java
@@ -0,0 +1,13 @@
+package com.example.xqm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XqmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XqmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xqm/XqmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xqm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XqmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
